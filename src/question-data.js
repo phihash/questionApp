7f3753c23723f8668d0847369f86f8f13f193806
phihash <insharp0220@gmail.com>
2022-02-21 19:29:02 +0900

    質問データサンプル作成

--- src/question-data.js
+++ src/question-data.js
@@ -1,6 +1,8 @@
 const questionData = [
-  {"id":1,"type":"input","questionText":"はなんですか","answer":"かにばりずむ","isSolved":false},
-  {"id":2,"type":"radio","questionText":"はなんですか","answer":[],"isSolved":false},
+  {"id":1,"type":"input","questionText":"はなんですか","answer":"かにばりずむ","Commentary":""},
+  {"id":2,"type":"check","questionText":"はなんですか","Choices":[],"answer":[],"Commentary":""},
+  {"id":3,"type":"radio","questionText":"はなんですか","Choices":[],"answer":[],"Commentary":""},
+  {"id":4,"type":"two","questionText":"はなんですか","Choices":[],"answer":[],"Commentary":""},
 ];
 
-export default questionData;
+export default questionData
